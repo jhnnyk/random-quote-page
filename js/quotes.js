17b08645cc1e57ab9a75a7e3f720da0dfb9792df
John Knotts <jhnnyk@gmail.com>
2016-11-17 08:34:37 -0700

    add quotes

---
+++ js/quotes.js
@@ -0,0 +1,24 @@
+var quotes = [
+  {
+    quote: 'You can do anything but not everything',
+    source: 'David Allen',
+    citation: 'Making It All Work',
+    year: 2009
+  },
+  {
+    quote: 'Life is 10% what happens to you and 90% how you react to it.',
+    source: 'Charles R. Swindoll'
+  },
+  {
+    quote: 'The secret of getting ahead is getting started.',
+    source: 'Mark Twain'
+  },
+  {
+    quote: 'It does not matter how slowly you go as long as you do not stop',
+    source: 'Confucius'
+  },
+  {
+    quote: 'Change your thoughts and you change your world.',
+    source: 'Norman Vincent Peale'
+  }
+];
