4408adf216e7e11a41b44db962dd570ff7d13767
John Knotts <jhnnyk@gmail.com>
2016-11-17 08:49:13 -0700

    console log random quote objects

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
+function getRandomQuote() {
+  var i = Math.floor(Math.random() * quotes.length);
+  return quotes[i];
+}
+
+function printQuote() {
+  console.log(getRandomQuote());
+}
+
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
 document.getElementById('loadQuote').addEventListener("click", printQuote, false);
-
