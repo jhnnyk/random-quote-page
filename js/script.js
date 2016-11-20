f37ab0a82a86c9502f8e60a7f83dd53f680567ed
John Knotts <jhnnyk@gmail.com>
2016-11-20 06:21:09 -0700

    dynamically load quote on page load

--- js/script.js
+++ js/script.js
@@ -24,12 +24,16 @@ function printQuote() {
   document.body.style.backgroundColor = getRandomColor();
 }
 
+// generate random color
 function getRandomColor() {
   var red   = Math.floor(Math.random() * 256);
   var green = Math.floor(Math.random() * 256);
   var blue  = Math.floor(Math.random() * 256);
   return 'rgb(' + red + ',' + green + ',' + blue + ')';
 }
+
+// load a quote right off the bat
+window.onload = printQuote();
 
 // event listener to respond to "Show another quote" button clicks
 // when user clicks anywhere on the button, the "printQuote" function is called
