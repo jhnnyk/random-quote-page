02b9793a978eeefbfaa0a71c64114678216113c7
John Knotts <jhnnyk@gmail.com>
2016-11-20 05:45:19 -0700

    add tags to the quote object

--- js/quotes.js
+++ js/quotes.js
@@ -3,32 +3,39 @@ var quotes = [
     quote: 'You can do anything but not everything',
     source: 'David Allen',
     citation: 'Making It All Work',
-    year: 2009
+    year: 2009,
+    tags: ['productivity', 'life']
   },
   {
     quote: 'Life is 10% what happens to you and 90% how you react to it.',
-    source: 'Charles R. Swindoll'
+    source: 'Charles R. Swindoll',
+    tags: ['attitude', 'life']
   },
   {
     quote: 'The secret of getting ahead is getting started.',
-    source: 'Mark Twain'
+    source: 'Mark Twain',
+    tags: ['motivation']
   },
   {
     quote: 'It does not matter how slowly you go as long as you do not stop',
-    source: 'Confucius'
+    source: 'Confucius',
+    tags: ['motivation']
   },
   {
     quote: 'Change your thoughts and you change your world.',
-    source: 'Norman Vincent Peale'
+    source: 'Norman Vincent Peale',
+    tags: ['attitude']
   },
   {
     quote: 'There is a crack, a crack, in everything. That\'s how the light gets in.',
     source: 'Leonard Cohen',
     citation: 'Anthem',
-    year: 1992
+    year: 1992,
+    tags: ['music', 'encouragement']
   },
   {
     quote: 'You miss 100% of the shots you never take.',
-    source: 'Wayne Gretzky'
+    source: 'Wayne Gretzky',
+    tags: ['attitude', 'fear', 'sports']
   }
 ];
